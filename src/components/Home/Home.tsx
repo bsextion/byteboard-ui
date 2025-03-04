d4b8c2be6c74fa00e739bacc21f8fc154a9ef269
Brooke Sextion <bsextion@gmail.com>
2025-03-03 21:33:40 -0600

    Added Sidebar and SearchBar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,32 @@
-import { autocompleteClasses, Box, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import Sidebar from "../Sidebar/Sidebar";
+import SearchBar from "./SearchBar";
 
 const Home = () => {
   return (
-    <Box sx={{ pt: 12, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-      <Typography variant="h2" sx={{my: 1, flexGrow: 0.5, fontWeight: "medium", color: '#32317E'}}>Built for Tech Professionals</Typography>
-      <Typography variant="h4" sx={{color: '#686EEC'}}>Job Searching Simplified</Typography>
+    <Box sx={{ display: "flex", flexDirection: "row", mt: 8, height: "90vh" }}>
+      <Sidebar />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          marginTop: 15,
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Typography
+          variant="h3"
+          sx={{ fontWeight: "medium", color: "#32317E" }}
+        >
+          Built for Tech Professionals
+        </Typography>
+        <Typography variant="h4" sx={{ color: "#686EEC" }}>
+          Job Searching Simplified
+        </Typography>
+        <SearchBar/>
+      </Box>
     </Box>
   );
 };
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import StarRateRoundedIcon from "@mui/icons-material/StarRateRounded";
 
-const Sidebar = () => {
+const drawerWidth = 60;
+
+export default function ResponsiveDrawer() {
+  const drawer = (
+    <div>
+      <List>
+        <ListItem key={1} disablePadding>
+          <ListItemButton divider={true}>
+            <ListItemIcon>
+              <AccountCircleRoundedIcon
+                sx={{
+                  color: "#EFF2FE",
+                }}
+              />
+            </ListItemIcon>
+            <ListItemText/>
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={2} disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <StarRateRoundedIcon sx={{ color: "#EFF2FE" }} />
+            </ListItemIcon>
+            <ListItemText />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </div>
+  );
+
   return (
-    <div>Sidebar</div>
-  )
+    <Box sx={{ display: "flex" }}>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+            zIndex: 1,
+            marginTop: 8,
+            backgroundColor: "#686EEC",
+          },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+    </Box>
+  );
 }
-
-export default Sidebar
